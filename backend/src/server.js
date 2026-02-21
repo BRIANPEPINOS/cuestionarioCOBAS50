4388f755a8ebcc69f9f52af8af6cb4fb1e76e402
BRIANPEPINOS <brpepinos@espe.edu.ec>
2026-02-21 01:11:49 -0500

    ajuste cors2

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-app.options("*", cors());
+app.options("/*", cors());
 app.use(express.json({ limit: "25mb" })); // imágenes base64 pueden ser pesadas
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
